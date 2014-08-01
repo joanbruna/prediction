36578576bb866505f0c8a34005d0ec86b8f34d42
psprechmann <pablo@128-122-47-224.dynapool.nyu.edu>
2014-08-01 10:08:46 -0400

    a

--- nmf_beta.m
+++ nmf_beta.m
@@ -1,38 +1,35 @@
-function [H,W] = nmf_beta(S,params) 
+function [H,W] = nmf_beta(S,W,params) 
 
 % beta, lambda1, lambda_ast, n_iter_max, tol, W , switch_W,H)
 
-V = abs(S).^params.p;
+V = abs(S);
 
-beta = params.beta;
-lambda1 = params.lambda1;
-lambda_ast = params.lambda_ast;
-tol = params.tol;
-W = params.W;
-n_iter_max = params.max_iter;
+beta = getoptions(params,'beta',2);
+Kn = getoptions(params,'Kn',2);
+lambda1 = getoptions(params,'lambda1',0.1);
+lambda_ast = getoptions(params,'lambda_ast',0);
+tol = getoptions(params,'tol',1e-6);
+n_iter_max = getoptions(params,'max_iter',200);
 
 % SCALE = 0;
 switch_H = 1;
-verbo = 0;
-if ~any(strcmp('switch_W',fieldnames(params)))
-    switch_W = 1;
-    
-end
-
+verbo = 1;
+switch_W = 1;
 
 [F,N] = size(V);
 [~,K] = size(W);
 
 if ~any(strcmp('H',fieldnames(params)))
-    H = abs(randn(K,N)) + 1;
+    H = abs(randn(K+Kn,N)) + 1;
 end
 
 
-V_ap = W*H;
+Wn = mexNormalize(abs(randn(F,Kn)));
 
-if length(lambda1)==1
-    lambda1 = lambda1*ones(K,1);
-end
+V_ap = [W,Wn]*H;
+
+lambda1 = lambda1*[ones(K,1);zeros(Kn,1)];
+
 
 
 Lambda1 = repmat(lambda1,1,N);
@@ -50,6 +47,7 @@ if tol>0
     err = Inf;
 end
 
+
 if (verbo == 1)
 fprintf('iter = %4i | obj = %+5.2E | err = %4.2E (target is %4.2E) \n',iter,obj(iter),err,tol)
 end
@@ -58,16 +56,16 @@ end
 while (iter < n_iter_max)    
     
     if switch_W
-        W = W .* ((V.*V_ap.^(beta-2))*H')./(V_ap.^(beta-1)*H'+lambda_ast*W+eps);
-        W(W(:)<eps) = 0;
+        V_ap_n = Wn*H((K+1):end,:);
+        Wn = Wn .* (((V - W*H(1:K,:)).*V_ap_n.^(beta-2))*H((K+1):end,:)')./(V_ap_n.^(beta-1)*H((K+1):end,:)'+eps);
         % normalize columns
-        W = projectW(W);
-        V_ap = W*H;
+        Wn = mexNormalize(Wn);
+        V_ap = [W,Wn]*H;
     end
     
     if switch_H
-        H = H .* (W'*(V.*V_ap.^(beta-2)))./(W'*V_ap.^(beta-1)+lambda_ast*H + Lambda1 + eps);
-        V_ap = W*H;
+        H = H .* ([W,Wn]'*(V.*V_ap.^(beta-2)))./([W,Wn]'*V_ap.^(beta-1)+lambda_ast*H + Lambda1 + eps);
+        V_ap = [W,Wn]*H;
         H(H(:)<eps) = 0;
     end
 
@@ -86,7 +84,7 @@ while (iter < n_iter_max)
     err = (obj(iter-1)-obj(iter))/obj(iter);
     end
     
-    if rem(iter,50)==0 && (verbo == 1)
+    if rem(iter,2)==0 && (verbo == 1)
         fprintf('iter = %4i | obj = %+5.2E | err = %4.2E (target is %4.2E) \n',iter,obj(iter),err,tol)
     end
         
